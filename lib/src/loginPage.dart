f3e90423e2a47ea23ec7793bf8542e6825af16d4
Sonu Sharma <sonu.sharma045@gmail.com>
2020-01-29 11:32:44 +0530

    Welcome and login page added

---
+++ lib/src/loginPage.dart
@@ -0,0 +1,218 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  Widget _backButton(){
+    return InkWell(
+      onTap: (){
+        Navigator.pop(context);
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child:  Row(
+          children: <Widget>[
+           Container(
+             padding: EdgeInsets.only(left: 0,top:10,bottom: 10),
+             child: Icon(Icons.keyboard_arrow_left,color:Colors.black),
+           ),
+           Text('Back',style:TextStyle(fontSize: 12, fontWeight: FontWeight.w500))
+          ],
+        ),
+      ),
+    );
+  }
+  Widget _entryField(String title){
+   return  Container(
+     margin: EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+          SizedBox(height: 10,),
+          TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              fillColor: Color(0xfff3f3f4),
+              filled: true)
+          )
+        ],
+      ),
+    );
+  } 
+  Widget _submitButton(){
+    return Container(
+      width : MediaQuery.of(context).size.width ,
+      padding: EdgeInsets.symmetric(vertical: 15),
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(5)),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+              color: Colors.grey.shade200,
+              offset: Offset(2, 4),
+              blurRadius: 5,
+              spreadRadius: 2
+          )
+        ],
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+          colors: [Color(0xfffbb448),Color(0xfff7892b)]
+        )
+      ),
+      child:  Text('Login',style:TextStyle(fontSize: 20,color: Colors.white),),
+    );
+  }
+  Widget _divider(){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        children: <Widget>[
+          SizedBox(width: 20,),
+          Expanded(
+            child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child:  Divider(thickness: 1,),
+            ),
+          ),
+          Text('or'),
+          Expanded(
+            child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Divider(thickness: 1,),
+            ),
+          ),
+          SizedBox(width: 20,),
+        ],
+      ),
+    );
+  }
+  Widget _facebookButton(){
+    return Container(
+       height: 50,
+      margin: EdgeInsets.symmetric(vertical: 20),
+      decoration: BoxDecoration(
+         borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+             
+              decoration: BoxDecoration(
+              color: Color(0xff1959a9),
+                 borderRadius: BorderRadius.only(bottomLeft: Radius.circular(5),topLeft: Radius.circular(5)),
+              ),
+              alignment: Alignment.center,
+              child: Text('f',style: TextStyle(color: Colors.white,fontSize:25,fontWeight: FontWeight.w400)),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Container(
+              decoration: BoxDecoration(
+              color: Color(0xff2872ba),
+                 borderRadius: BorderRadius.only(bottomRight: Radius.circular(5),topRight: Radius.circular(5)),
+              ),
+              alignment: Alignment.center,
+              child: Text('Log in with Facebook',style: TextStyle(color: Colors.white,fontSize:18,fontWeight: FontWeight.w400)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  Widget _createAccountLabel(){
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Don\'t have an account ?',style: TextStyle(fontSize: 13,fontWeight: FontWeight.w500),),
+          SizedBox(width: 10,),
+          Text('Register',style: TextStyle(color: Color(0xfff79c4f),fontSize: 13,fontWeight: FontWeight.w500),)
+        ],
+      ),
+    );
+  }
+  Widget _title(){
+    return RichText(
+        textAlign: TextAlign.center,
+        text:TextSpan(
+          text: 'd',
+          style: TextStyle(color: Color(0xffe46b10), fontSize: 30, ),
+          children: [
+            TextSpan(text: 'ev',
+            style: TextStyle(
+              color: Colors.black, fontSize: 30),
+             
+            ),
+            TextSpan(text: 'rnz',
+            style: TextStyle(
+              color: Color(0xffe46b10), fontSize: 30),
+             
+            ),
+          ]
+        ),
+      );
+  }
+  Widget _emailPasswordWidget(){
+    return Column(
+        children: <Widget>[
+          _entryField("Email id"),
+                _entryField("Password"),
+        ],
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _title(),
+                 _emailPasswordWidget(),
+                  SizedBox(height: 20,),
+                 _submitButton(),
+                 Container(
+                   padding: EdgeInsets.symmetric(vertical: 10),
+                   alignment: Alignment.centerRight,
+                   child: Text('Forgot Password ?',style:TextStyle(fontSize: 14,fontWeight: FontWeight.w500)),
+                 ),
+                 _divider(),
+                 _facebookButton(),
+                
+              ],
+            ),
+          ),
+          Align(
+           alignment: Alignment.bottomCenter,
+            child:  _createAccountLabel(),
+          ),
+          Positioned(
+            top: 40,
+            left: 0,
+            child: _backButton()
+          )
+        ],
+      )
+       ,
+   );
+  }
+}
---
+++ lib/src/welcomePage.dart
@@ -0,0 +1,141 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_login_signup/src/loginPage.dart';
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  
+ 
+  Widget _submitButton(){
+    return InkWell(
+      onTap: (){
+         Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width : MediaQuery.of(context).size.width ,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Color(0xffdf8e33).withAlpha(100),
+                offset: Offset(2, 4),
+                blurRadius: 8,
+                spreadRadius: 2
+            )
+          ],
+          color: Colors.white
+        ),
+        child:  Text('Login',style:TextStyle(fontSize: 20,color: Color(0xfff7892b)),),
+      ),
+    );
+  }
+   Widget _signUpButton(){
+    return InkWell(
+      onTap: (){
+         Navigator.push(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+      },
+      child: Container(
+        width : MediaQuery.of(context).size.width ,
+        padding: EdgeInsets.symmetric(vertical: 13),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child:  Text('Register now',style:TextStyle(fontSize: 20,color: Colors.white),),
+      ),
+    );
+  }
+  Widget _label(){
+    return Container(
+      margin: EdgeInsets.only(top:40,bottom: 20),
+      child: Column(
+        children: <Widget>[
+          Text('Quick login with Touch ID',style: TextStyle(color: Colors.white, fontSize: 17),),
+          SizedBox(height: 20,),
+          Icon(Icons.fingerprint,size:90,color:Colors.white),
+          SizedBox(height: 20,),
+          Text('Touch ID',style: TextStyle(color: Colors.white, fontSize: 15, decoration: TextDecoration.underline,), ),
+        ],
+      )
+    );
+  }
+  Widget _title(){
+    return RichText(
+        textAlign: TextAlign.center,
+        text:TextSpan(
+          text: 'd',
+          style: TextStyle(color: Colors.white, fontSize: 30, ),
+          children: [
+            TextSpan(text: 'ev',
+            style: TextStyle(
+              color: Colors.black, fontSize: 30),
+             
+            ),
+            TextSpan(text: 'rnz',
+            style: TextStyle(
+              color: Colors.white, fontSize: 30),
+             
+            ),
+          ]
+        ),
+      );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     
+      body: Stack(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.shade200,
+                    offset: Offset(2, 4),
+                    blurRadius: 5,
+                    spreadRadius: 2
+                )
+              ],
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xfffbb448),Color(0xffe46b10)]
+              )
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                 _title(),
+                 SizedBox(height: 80,),
+                 _submitButton(),
+                 SizedBox(height: 20,),
+                _signUpButton(),
+                 SizedBox(height: 20,),
+                _label()
+               
+              ],
+            ),
+          ),
+        ],
+      )
+       ,
+   );
+  }
+}
